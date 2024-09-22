a7c6b2e1faa4e27f3d8ba0d2e80b5e7c9fcbc9f3
Grzesiek1982 <grzesieg1982@interia.pl>
2024-09-22 22:29:57 +0200

    Zmiany w pliku index.js - testy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const element = React.createElement(
+  'a',
+  { href: 'https://google.pl' },
+  'Google'
 );
+
+console.log(element);
+
+ReactDOM.createRoot(document.getElementById('root')).render(element);
