7c8ab1ecb49675abbc38b62ef08f2e773c36b2d6
Grzesiek1982 <grzesieg1982@interia.pl>
2024-09-29 19:12:06 +0200

    Dodanie pliku Profile.jsx oraz Profile.css

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,45 @@
+import styles from './Profile.styles.css';
+
+const Profile = ({ name, tag, location, avatar, stats }) => {
+  return (
+    <div>
+      <div className={styles.profile}>
+        <img src={avatar} alt="User avatar" />
+        <p className={styles.name}>{name}</p>
+        <p>{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
+
+// {
+//   /* <div class="profile">
+//   <div class="description">
+//     <img
+//       src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+//       alt="User avatar"
+//       class="avatar"
+//     />
+//     <p class="name">Petra Marica</p>
+//     <p class="tag">@pmarica</p>
+//     <p class="location">Salvador, Brasil</p>
+//   </div> */
+// }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/App';
+// import './index.css';
 
-const element = React.createElement(
-  'a',
-  { href: 'https://google.pl' },
-  'Google'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
-console.log(element);
-
-ReactDOM.createRoot(document.getElementById('root')).render(element);
